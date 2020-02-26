ba7102bea75f19481635e174513294f566f991c4
Vikas Jha <Vikas.Jha@parkar.local>
2020-02-26 13:24:22 +0530

    v1.0

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Welcome to My Website testing</p>'; ?> 
+ <?php echo '<p>Welcome to My Website testing- Version v1.0</p>'; ?> 
  </body>
 </html>
